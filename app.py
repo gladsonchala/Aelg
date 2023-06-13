0af82454651c317c23e6fe4b189881865e41b069
Gemechis Chala Degefa <117309484+gladsonchala@users.noreply.github.com>
2023-06-14 01:50:25 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,6 @@
+#base_url = "https://us-east-2.aws.data.mongodb-api.com/app/data-vagob/endpoint/data/v1"  # Replace with your MongoDB Data API endpoint
+#api-key = "WIAsG0L4dV89Jg1ZGNfUEYFhuBZ4XeXdpbj3vdb4KQMGagsSTijtGBtdNRocUp7x"  # Replace with your MongoDB Data API key
+
 from flask import Flask, Response, request, jsonify
 import json
 from bson.objectid import ObjectId
@@ -5,11 +8,13 @@ import requests
 
 app = Flask(__name__)
 
-base_url = "https://us-east-2.aws.data.mongodb-api.com/app/data-vagob/endpoint/data/v1"  # Replace with your MongoDB Data API endpoint
+# MongoDB Data API configuration
+base_url = "https://us-east-2.aws.data.mongodb-api.com/app/data-vagob/endpoint/data/v1"
 headers = {
     "Content-Type": "application/json",
-    "api-key": "WIAsG0L4dV89Jg1ZGNfUEYFhuBZ4XeXdpbj3vdb4KQMGagsSTijtGBtdNRocUp7x"  # Replace with your MongoDB Data API key
+    "Authorization": "Bearer WIAsG0L4dV89Jg1ZGNfUEYFhuBZ4XeXdpbj3vdb4KQMGagsSTijtGBtdNRocUp7x"
 }
+
 
 # Create a job
 @app.route("/jobs", methods=["POST"])
@@ -38,7 +43,7 @@ def create_job():
             }
             job["photos"].append(photo)
 
-        # Make a request to MongoDB Data API to insert the job
+        # Make a request to MongoDB Data API to create the job
         url = f"{base_url}/insertOne"
         payload = json.dumps({
             "database": "agelgilotify",
@@ -48,7 +53,7 @@ def create_job():
         response = requests.post(url, headers=headers, data=payload)
 
         if response.status_code == 200:
-            job_id = response.json().get("insertedId")
+            job_id = response.json()["insertedId"]
             return Response(
                 response=json.dumps(
                     {"message": "Job created", "id": str(job_id)}
@@ -72,6 +77,7 @@ def create_job():
             mimetype="application/json"
         )
 
+
 # Get all jobs
 @app.route("/jobs", methods=["GET"])
 def get_jobs():
@@ -80,12 +86,13 @@ def get_jobs():
         url = f"{base_url}/find"
         payload = json.dumps({
             "database": "agelgilotify",
-            "collection": "jobs"
+            "collection": "jobs",
+            "limit": 100
         })
         response = requests.post(url, headers=headers, data=payload)
 
         if response.status_code == 200:
-            jobs = response.json().get("documents")
+            jobs = response.json()["documents"]
             for job in jobs:
                 job["_id"] = str(job["_id"])
             return Response(
@@ -95,7 +102,7 @@ def get_jobs():
             )
         else:
             return Response(
-                response=json.dumps({"message": "Error fetching jobs"}),
+                response=json.dumps({"message": "Failed to fetch jobs"}),
                 status=500,
                 mimetype="application/json"
             )
@@ -108,6 +115,7 @@ def get_jobs():
             status=500,
             mimetype="application/json"
         )
+
 
 # Get a job by ID
 @app.route("/jobs/<job_id>", methods=["GET"])
@@ -123,22 +131,29 @@ def get_job(job_id):
         response = requests.post(url, headers=headers, data=payload)
 
         if response.status_code == 200:
-            job = response.json().get("document")
-            job["_id"] = str(job["_id"])
-            return Response(
-                response=json.dumps(job),
-                status=200,
-                mimetype="application/json"
-            )
+            job = response.json()["document"]
+            if job:
+                job["_id"] = str(job["_id"])
+                return Response(
+                    response=json.dumps(job),
+                    status=200,
+                    mimetype="application/json"
+                )
+            else:
+                return Response(
+                    response=json.dumps({"message": "Job not found"}),
+                    status=404,
+                    mimetype="application/json"
+                )
         else:
             return Response(
-                response=json.dumps({"message": "Job not found"}),
-                status=404,
+                response=json.dumps({"message": "Failed to fetch the job"}),
+                status=500,
                 mimetype="application/json"
             )
 
     except Exception as ex:
-        error_message = "Error fetching job: " + str(ex)
+        error_message = "Error fetching a job: " + str(ex)
         print(error_message)
         return Response(
             response=json.dumps({"message": error_message}),
@@ -146,43 +161,33 @@ def get_job(job_id):
             mimetype="application/json"
         )
 
+
 # Update a job
 @app.route("/jobs/<job_id>", methods=["PUT"])
 def update_job(job_id):
     try:
-        job_updates = {}
+        updated_job = {
+            "title": request.form["title"],
+            "description": request.form["description"],
+            "category": request.form["category"],
+            "location": request.form["location"],
+            "price": float(request.form["price"]),
+            "duration": request.form["duration"],
+            "status": request.form["status"],
+            "is_emergency": bool(request.form.get("is_emergency")),
+            "task_giver_id": ObjectId(request.form["task_giver_id"]),
+            "photos": []
+        }
 
-        if "title" in request.form:
-            job_updates["title"] = request.form["title"]
-        if "description" in request.form:
-            job_updates["description"] = request.form["description"]
-        if "category" in request.form:
-            job_updates["category"] = request.form["category"]
-        if "location" in request.form:
-            job_updates["location"] = request.form["location"]
-        if "price" in request.form:
-            job_updates["price"] = float(request.form["price"])
-        if "duration" in request.form:
-            job_updates["duration"] = request.form["duration"]
-        if "status" in request.form:
-            job_updates["status"] = request.form["status"]
-        if "is_emergency" in request.form:
-            job_updates["is_emergency"] = bool(request.form.get("is_emergency"))
-        if "task_giver_id" in request.form:
-            job_updates["task_giver_id"] = ObjectId(request.form["task_giver_id"])
-
-        # Handle multiple photo updates
+        # Handle multiple photos
         photo_urls = request.form.getlist("photos_url")
         photo_captions = request.form.getlist("photos_caption")
-        if len(photo_urls) > 0 and len(photo_captions) > 0:
-            photos = []
-            for url, caption in zip(photo_urls, photo_captions):
-                photo = {
-                    "url": url,
-                    "caption": caption
-                }
-                photos.append(photo)
-            job_updates["photos"] = photos
+        for url, caption in zip(photo_urls, photo_captions):
+            photo = {
+                "url": url,
+                "caption": caption
+            }
+            updated_job["photos"].append(photo)
 
         # Make a request to MongoDB Data API to update the job
         url = f"{base_url}/updateOne"
@@ -190,25 +195,32 @@ def update_job(job_id):
             "database": "agelgilotify",
             "collection": "jobs",
             "filter": {"_id": ObjectId(job_id)},
-            "update": {"$set": job_updates}
+            "update": {"$set": updated_job}
         })
         response = requests.post(url, headers=headers, data=payload)
 
         if response.status_code == 200:
-            return Response(
-                response=json.dumps({"message": "Job updated"}),
-                status=200,
-                mimetype="application/json"
-            )
+            if response.json()["matchedCount"] > 0:
+                return Response(
+                    response=json.dumps({"message": "Job updated"}),
+                    status=200,
+                    mimetype="application/json"
+                )
+            else:
+                return Response(
+                    response=json.dumps({"message": "Job not found"}),
+                    status=404,
+                    mimetype="application/json"
+                )
         else:
             return Response(
-                response=json.dumps({"message": "Job not found"}),
-                status=404,
+                response=json.dumps({"message": "Failed to update the job"}),
+                status=500,
                 mimetype="application/json"
             )
 
     except Exception as ex:
-        error_message = "Error updating job: " + str(ex)
+        error_message = "Error updating a job: " + str(ex)
         print(error_message)
         return Response(
             response=json.dumps({"message": error_message}),
@@ -216,11 +228,12 @@ def update_job(job_id):
             mimetype="application/json"
         )
 
+
 # Delete a job
 @app.route("/jobs/<job_id>", methods=["DELETE"])
 def delete_job(job_id):
     try:
-        # Make a request to MongoDB Data API to delete the job by ID
+        # Make a request to MongoDB Data API to delete the job
         url = f"{base_url}/deleteOne"
         payload = json.dumps({
             "database": "agelgilotify",
@@ -230,20 +243,27 @@ def delete_job(job_id):
         response = requests.post(url, headers=headers, data=payload)
 
         if response.status_code == 200:
-            return Response(
-                response=json.dumps({"message": "Job deleted"}),
-                status=200,
-                mimetype="application/json"
-            )
+            if response.json()["deletedCount"] > 0:
+                return Response(
+                    response=json.dumps({"message": "Job deleted"}),
+                    status=200,
+                    mimetype="application/json"
+                )
+            else:
+                return Response(
+                    response=json.dumps({"message": "Job not found"}),
+                    status=404,
+                    mimetype="application/json"
+                )
         else:
             return Response(
-                response=json.dumps({"message": "Job not found"}),
-                status=404,
+                response=json.dumps({"message": "Failed to delete the job"}),
+                status=500,
                 mimetype="application/json"
             )
 
     except Exception as ex:
-        error_message = "Error deleting job: " + str(ex)
+        error_message = "Error deleting a job: " + str(ex)
         print(error_message)
         return Response(
             response=json.dumps({"message": error_message}),
@@ -251,5 +271,99 @@ def delete_job(job_id):
             mimetype="application/json"
         )
 
+
+# Apply for a job
+@app.route("/jobs/apply", methods=["POST"])
+def apply_for_job():
+    try:
+        application = {
+            "job_id": ObjectId(request.form["job_id"]),
+            "user_id": ObjectId(request.form["user_id"]),
+            "status": "Pending",
+            "created_at": datetime.now().strftime("%Y-%m-%d %H:%M:%S"),
+            "rating": None
+        }
+
+        # Make a request to MongoDB Data API to insert the application
+        url = f"{base_url}/insertOne"
+        payload = json.dumps({
+            "database": "agelgilotify",
+            "collection": "applications",
+            "document": application
+        })
+        response = requests.post(url, headers=headers, data=payload)
+
+        if response.status_code == 200:
+            if response.json()["insertedId"]:
+                return Response(
+                    response=json.dumps(
+                        {"message": "Application submitted", "id": str(response.json()["insertedId"])}
+                    ),
+                    status=200,
+                    mimetype="application/json"
+                )
+            else:
+                return Response(
+                    response=json.dumps({"message": "Failed to submit the application"}),
+                    status=500,
+                    mimetype="application/json"
+                )
+        else:
+            return Response(
+                response=json.dumps({"message": "Failed to submit the application"}),
+                status=500,
+                mimetype="application/json"
+            )
+
+    except Exception as ex:
+        error_message = "Error applying for a job: " + str(ex)
+        print(error_message)
+        return Response(
+            response=json.dumps({"message": error_message}),
+            status=500,
+            mimetype="application/json"
+        )
+
+
+# Get applications for a job
+@app.route("/jobs/<job_id>/applications", methods=["GET"])
+def get_job_applications(job_id):
+    try:
+        # Make a request to MongoDB Data API to get the applications for the job
+        url = f"{base_url}/find"
+        payload = json.dumps({
+            "database": "agelgilotify",
+            "collection": "applications",
+            "filter": {"job_id": ObjectId(job_id)}
+        })
+        response = requests.post(url, headers=headers, data=payload)
+
+        if response.status_code == 200:
+            applications = response.json()["documents"]
+            for application in applications:
+                application["_id"] = str(application["_id"])
+            return Response(
+                response=json.dumps(applications),
+                status=200,
+                mimetype="application/json"
+            )
+        else:
+            return Response(
+                response=json.dumps({"message": "Failed to fetch the applications"}),
+                status=500,
+                mimetype="application/json"
+            )
+
+    except Exception as ex:
+        error_message = "Error fetching the applications: " + str(ex)
+        print(error_message)
+        return Response(
+            response=json.dumps({"message": error_message}),
+            status=500,
+            mimetype="application/json"
+        )
+
+
 if __name__ == "__main__":
     app.run(debug=True)
+
